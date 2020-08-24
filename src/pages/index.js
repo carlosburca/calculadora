b79480da286ed617861aad6b315cfbd31b5a60a4
Carlos Burgos <cburgos@abstrahere.com>
2020-08-23 20:27:42 -0400

    prueba

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,22 +1,43 @@
-import React from "react"
-import { Link } from "gatsby"
+import React, { useState } from "react"
 
-import Layout from "../components/layout"
-import Image from "../components/image"
-import SEO from "../components/seo"
+const IndexPage = () => {
+  const [num1, setNum1] = useState(0)
+  const handleNum1 = (value) => setNum1(value)
+  const [num2, setNum2] = useState(0)
+  const handleNum2 = (value) => setNum2(value)
 
-const IndexPage = () => (
-  <Layout>
-    <SEO title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
-    </div>
-    <Link to="/page-2/">Go to page 2</Link> <br />
-    <Link to="/using-typescript/">Go to "Using TypeScript"</Link>
-  </Layout>
-)
+  const [resultado, setResultado] = useState('')
+
+  const sumar = () => {
+    const res = parseInt(num1) + parseInt(num2);
+    setResultado(res)
+  }
+  const restar = () => {
+    const res = parseInt(num1) - parseInt(num2);
+    setResultado(res)
+  }
+  const reset = () => {
+    setNum1(0)
+    setNum2(0)
+    setResultado('')
+  }
+  return (
+    <>
+      <h1>Prueba</h1>
+      <input type="number" min="0" onChange={e => handleNum1(e.target.value)} value={num1} /> <br />
+
+      <input type="number" min="0" onChange={e => handleNum2(e.target.value)} value={num2} /><br />
+      <button onClick={() => sumar()}>Sumar</button>
+      <button onClick={() => restar()}>Restar</button>
+      <button onClick={() => reset()}>Reset</button>
+      <div>
+        resultado: {resultado}
+      </div>
+
+    </>
+
+
+  )
+}
 
 export default IndexPage
